23334a90bbc63d423a57b5158ecbb31889dc05ad
Adithyan S Dev <151626372+AdithyanSDev@users.noreply.github.com>
2024-02-26 16:52:06 +0530

    Time: 49 ms (97.44%) | Memory: 51.1 MB (20.73%) - LeetSync

---
+++ 3246-check-if-bitwise-or-has-trailing-zeros/check-if-bitwise-or-has-trailing-zeros.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var hasTrailingZeros = function(nums) {
+    let count =0;
+    for ( let i =0;i<nums.length;i++){
+        if(nums[i]%2==0){
+            count++
+        }
+        if(count>=2){
+          return true  
+        }
+    }
+    return false
+};
