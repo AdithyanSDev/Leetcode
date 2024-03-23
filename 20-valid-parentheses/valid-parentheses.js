c72a8e88ac374c960ba9ce451a9080bd2579c5db
Adithyan S Dev <151626372+AdithyanSDev@users.noreply.github.com>
2024-03-23 18:00:53 +0530

    Time: 56 ms (64.91%) | Memory: 50.8 MB (26.06%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        switch (s[i]) {
+            case '(':
+            case '[':
+            case '{':
+                stack.push(s[i]);
+                break;
+            case ')':
+                if (stack.pop() !== '(') return false;
+                break;
+            case ']':
+                if (stack.pop() !== '[') return false;
+                break;
+            case '}':
+                if (stack.pop() !== '{') return false;
+                break;
+            default:
+               
+                return false;
+        }
+    }
+   
+    return stack.length === 0;
+};
