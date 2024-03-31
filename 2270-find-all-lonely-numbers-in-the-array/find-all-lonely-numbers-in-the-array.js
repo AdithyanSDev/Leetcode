a03cf5d1d99aec0c79e0956ea734765eac102ba5
Adithyan S Dev <151626372+AdithyanSDev@users.noreply.github.com>
2024-03-31 14:57:14 +0530

    Time: 284 ms (88.14%) | Memory: 79.3 MB (100.00%) - LeetSync

---
+++ 2270-find-all-lonely-numbers-in-the-array/find-all-lonely-numbers-in-the-array.js
@@ -0,0 +1,10 @@
+var findLonely = function(nums) {
+    let result = [];
+
+    nums.sort((a,b) => a-b);
+    for(let i=0;i<nums.length;i++) {
+        if(nums[i] !== nums[i-1]+1 && nums[i] !== nums[i+1]-1 && nums[i] !== nums[i-1] && nums[i] !== nums[i+1])
+            result.push(nums[i]);
+    }
+    return result;
+};
