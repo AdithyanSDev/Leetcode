8a64a7a4618ab1fa773e7b5350f2334be08d347b
Adithyan S Dev <151626372+AdithyanSDev@users.noreply.github.com>
2024-04-11 15:21:17 +0530

    Time: 44 ms (92.12%) | Memory: 48.8 MB (80.77%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    var start=0
+    var end=nums.length-1
+        while(start<=end){
+          var mid=Math.floor((start+end)/2)
+          if(target===nums[mid]){
+            return mid
+          }else if(target<nums[mid]){
+            end=mid-1
+          }else{
+            start=mid+1
+          }
+        }
+   return start
+    };
+  
