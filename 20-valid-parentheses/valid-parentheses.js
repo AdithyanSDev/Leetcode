3bf54b4b5e9799561b1525c4629a2a2c60526944
Adithyan S Dev <151626372+AdithyanSDev@users.noreply.github.com>
2024-04-30 12:21:46 +0530

    Time: 62 ms (32.81%) | Memory: 50.2 MB (55.97%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -3,28 +3,13 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    let stack = [];
-    for (let i = 0; i < s.length; i++) {
-        switch (s[i]) {
-            case '(':
-            case '[':
-            case '{':
-                stack.push(s[i]);
-                break;
-            case ')':
-                if (stack.pop() !== '(') return false;
-                break;
-            case ']':
-                if (stack.pop() !== '[') return false;
-                break;
-            case '}':
-                if (stack.pop() !== '{') return false;
-                break;
-            default:
-               
-                return false;
-        }
-    }
-   
-    return stack.length === 0;
+  let stack=[]
+   for(let i=0;i<s.length;i++){
+     if(s[i]==='('||s[i]==='['||s[i]==='{') stack.push(s[i])
+        else if(stack.length===0) return false
+       else if(s[i]===')'&&stack.pop()!=='(') return false
+       else if(s[i]===']'&&stack.pop()!=='[') return false
+       else if(s[i]==='}'&&stack.pop()!=='{') return false
+   }
+   return stack.length===0
 };
