6154d3e1edfc79b60d5363d8737c7ff901db8b2d
Adithyan S Dev <151626372+AdithyanSDev@users.noreply.github.com>
2024-05-08 18:55:40 +0530

    Time: 111 ms (25.01%) | Memory: 49.6 MB (70.91%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,15 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    let j = i + 1;
-    while (j < nums.length) {
-      const result = nums[i] + nums[j];
-      if (result === target) {
-        return [i, j];
-      }
-      j++;
+  for(let i=0;i<nums.length;i++){
+    for(let j=i+1;j<nums.length;j++){
+        if(nums[i]+nums[j]===target){
+            return [i,j ]
+        }
     }
   }
-  return null; 
+  return null
 };
